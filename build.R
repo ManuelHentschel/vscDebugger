32dad9a33d98aaf06a458d6d456e3fb689762527
Kun Ren <renkun@outlook.com>
2020-05-29 21:20:17 +0800

    Update build.R

--- build.R
+++ build.R
@@ -1,2 +1,2 @@
 devtools::document()
-devtools::install(args = c('--library=../VSCode-R-Debugger/test/R/lib', '--no-lock'), reload = FALSE)
+devtools::install(args = c('--no-lock'), reload = FALSE, build = FALSE)
