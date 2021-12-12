077472782fbd3e8fe7e1a973d8386d2993e12a65
ManuelHentschel <53863351+ManuelHentschel@users.noreply.github.com>
2021-12-12 13:52:42 +0100

    changes

--- R/tclServer.R
+++ R/tclServer.R
@@ -1,6 +1,58 @@
 
 
 
+#' @export
+.vsc.startWebsocket <- function(
+  port = session$dapPort,
+  host = '127.0.0.1'
+){
+  cat('Starting server on port: ', port, '\n', sep='')
+  session$dapPort <- port
+  session$socketServer <- startServer(
+    port = port,
+    cb = .vsc.handleDap
+  )
+}
+
+
+#' @export
+.vsc.handleDap <- function(s, socket='tclSocket', ...){
+  
+  # SKIPPED_FRAMES <- 6
+  SKIPPED_FRAMES <- 1
+
+  registerEntryFrame(SKIPPED_FRAMES)
+
+  session$svName <- socket
+  s <- paste0(session$restOfWs, s)
+
+  while(TRUE){
+    # identify content-length
+    m <- regexec('^Content-Length: (\\d+)\r?\n\r?\n', s)
+    rm <- regmatches(s, m)
+
+    if(length(rm[[1]]) == 0){
+      session$restOfWs <- s
+      break
+    }
+
+    contentLength <- as.numeric(rm[[1]][2])
+    headerLength <- nchar(rm[[1]][1])
+
+    if(nchar(s)<contentLength+headerLength){
+      session$restOfWs <- s
+      break
+    }
+
+    json <- substring(s, headerLength+1, headerLength + contentLength)
+    s <- substring(s, headerLength+contentLength+1, length(s))
+
+    .vsc.handleJson(json)
+  }
+  unregisterEntryFrame(SKIPPED_FRAMES)
+  return('')
+}
+
 
 startServer <- function(port, cb){
   cbFunc <- function(){
