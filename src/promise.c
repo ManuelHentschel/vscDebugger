e3411429d5d69ecb2df197316a29067563a9db1b
Manuel <53863351+ManuelHentschel@users.noreply.github.com>
2026-07-06 12:25:12 +0200

    Fix promise handling in R>=4.6.0

--- src/promise.c
+++ src/promise.c
@@ -63,8 +63,52 @@ SEXP attribute_hidden c_promise_info(SEXP sym, SEXP env) {
 
 #else
 
-// todo:
-// figure out how to do the same as above, using
+// for R >= 4.6.0, use api from:
 // https://cran.r-project.org/doc/manuals/r-release/R-exts.html#Working-with-variable-bindings
+
+SEXP attribute_hidden c_is_promise(SEXP sym, SEXP env) {
+  if (!isSymbol(sym)) {
+    error("'sym' must be a symbol");
+  }
+  if (!isEnvironment(env)) {
+    error("'env' must be an environment");
+  }
+  return ScalarLogical(R_GetBindingType(sym, env) == R_BindingTypeDelayed);
+}
+
+SEXP attribute_hidden c_promise_info(SEXP sym, SEXP env) {
+  if (!isSymbol(sym)) {
+    error("'sym' must be a symbol");
+  }
+  if (!isEnvironment(env)) {
+    error("'env' must be an environment");
+  }
+  if (R_GetBindingType(sym, env) != R_BindingTypeDelayed){
+    error("The object is not a promise");
+  }
+
+  // Get proimse expression and environment
+  SEXP exp = R_DelayedBindingExpression(sym, env);
+  SEXP promise_env = R_DelayedBindingEnvironment(sym, env);
+
+  int len = 2;
+
+  /* allocate and populate list */
+  SEXP ret = PROTECT(allocVector(VECSXP, len));
+  SET_VECTOR_ELT(ret, 0, exp);
+  SET_VECTOR_ELT(ret, 1, promise_env);
+
+  /* create names */
+  SEXP nms = PROTECT(allocVector(STRSXP, len));
+  SET_STRING_ELT(nms, 0, mkChar("code"));
+  SET_STRING_ELT(nms, 1, mkChar("environment"));
+
+  /* assign names to list */
+  setAttrib(ret, R_NamesSymbol, nms);
+
+  /* cleanup and return */
+  UNPROTECT(2);
+  return ret;
+}
 
 #endif
--- src/promise.h
+++ src/promise.h
@@ -8,7 +8,13 @@
 #include <R_ext/Error.h>
 #include <Rversion.h>
 
+
+#if R_VERSION < R_Version(4, 6, 0)
 SEXP attribute_hidden c_is_promise(SEXP, SEXP, SEXP);
+#else
+SEXP attribute_hidden c_is_promise(SEXP, SEXP);
+#endif
+
 SEXP attribute_hidden c_promise_info(SEXP, SEXP);
 
 #endif
